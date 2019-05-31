90e7f491b611ab1be0cb716790153ab67d064ebc
fconstancias <florentinconstancias@gmail.com>
2019-05-31 12:02:19 +0200

    Create 1_trimmomatic.sh

---
+++ 1_trimmomatic.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#$ -q normal.q
+#$ -N 1_trimmomatic
+#$ -M florentin.constancias@cirad.fr
+#$ -pe parallel_smp 6
+#$ -l mem_free=6G
+#$ -V
+#$ -cwd
+
+module purge
+module load system/conda/5.1.0
+
+source activate trimmomatic
+
+IN=IN
+OUT=trimmomatic
+
+mkdir -p ${OUT}
+
+for NAME in `awk '{print $1}' sample_names.tsv`
+do
+ls ${IN}${NAME}*R1*gz ${IN}${NAME}*R2*gz
+
+trimmomatic PE -phred33 \
+	${IN}${NAME}*R1*gz ${IN}${NAME}*R2*gz \
+	${OUT}${NAME}_L001_R1_001.fastq.gz ${OUT}${NAME}_L001_R1_001_unpaired.fastq.gz \
+	${OUT}${NAME}_L001_R2_001.fastq.gz ${OUT}${NAME}_L001_R2_001_unpaired.fastq.gz \
+	ILLUMINACLIP:/homedir/constancias/.conda/envs/trimmomatic/share/trimmomatic-0.39-1/adapters/TruSeq3-PE.fa:2:30:10 \
+	LEADING:0 TRAILING:0 \
+	SLIDINGWINDOW:4:15 MINLEN:50 \
+	-threads ${NSLOTS} \
+	-trimlog ${OUT}${NAME}_trimmomatic_log.txt
+
+done
+
+# JOB END
+date
+
+exit 0
+
